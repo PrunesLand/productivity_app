75070cb6414b6e8f2c1a96dbe957d57add46a074
Pranaya Anargya <pranaya.anargya2@homecredit.co.id>
2024-07-26 13:24:56 +0700

    First push

---
+++ productivity_reminder_app/productivity_reminder_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  productivity_reminder_appApp.swift
+//  productivity_reminder_app
+//
+//  Created by Pranaya Anargya (ID) on 26/07/24.
+//
+
+import SwiftUI
+
+@main
+struct productivity_reminder_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
